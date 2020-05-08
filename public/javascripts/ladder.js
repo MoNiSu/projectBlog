8bbda1914d31d0c379a9cda3b4c1d61e90f50301
monisu_mac <xogns712015@gmail.com>
2020-05-08 16:59:19 +0900

    Update ladder page (add ladder bridge)

--- public/javascripts/ladder.js
+++ public/javascripts/ladder.js
@@ -7,15 +7,43 @@ const canvas = document.getElementById('jsLadder');
 
 const ctx = canvas.getContext('2d');
 
+function percent (num) {
+	return Math.round(Math.random() * num);
+}
+
 var people = 2;
 
 peopleNumber.innerHTML = people;
 
 function drawLadder () {
-	function ladderLinePlus (height) {
+	let line = 100;
+
+	let xPlus = Math.floor(550 / (people - 1));
+
+	function ladderLinePlus (x) {
 		ctx.beginPath();
-		ctx.moveTo(height, 80);
-		ctx.lineTo(height, 270);
+
+		ctx.moveTo(x, 80);
+
+		let y = 81;
+
+		for (let i = 1; i <= 3; i++) {
+			y += 47;
+			ctx.lineTo(x, y);
+			if (percent(10) < 6) {
+				if (percent(10) > 6) {
+					ctx.lineTo(x + xPlus, y + 20);
+					ctx.moveTo(x, y);
+				} else if (percent(10) > 2) {
+					ctx.lineTo(x + xPlus, y);
+					ctx.moveTo(x, y);
+				} else {
+					ctx.lineTo(x + xPlus, y - 20);
+					ctx.moveTo(x, y);
+				}
+			}
+		}
+		ctx.lineTo(x, 270);
 		ctx.stroke();
 	}
 
@@ -26,20 +54,20 @@ function drawLadder () {
 		ctx.stroke();
 	}
 
-	let line = 100;
-	for (let i = 1; i <= people; i++) {
+
+	for (let j = 1; j <= people; j++) {
 		ctx.lineWidth = 3;
 		ctx.strokeStyle = 'rgb(0, 0, 0)';
 
-		if (i < people) {
+		if (j < people) {
 			ctx.strokeRect(line - 30, 40, 60, 30);
 
 			ladderLinePlus(line);
 
 			ctx.strokeRect(line - 30, 280, 60, 30);
 
-			line += Math.floor((750 - 200) / (people - 1));
-		} else if (i === people) {
+			line += xPlus;
+		} else if (j === people) {
 			ctx.strokeRect(line - 30, 40, 60, 30);
 
 			ladderLine(line);
