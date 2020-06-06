3fd20df52bb8f727689e433155fff9baefffc50b
monisu_windows <xogns712015@gmail.com>
2020-06-06 12:27:30 +0900

    Update auth to korean

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,9 @@ router.use(session(sessionAuth));
 
 router.get('/', function (req, res) {
 	if (req.session.username) {
-		res.render('pages/index', { title: 'Home', username: req.session.username + '님', authLocation: 'signout', auth: 'Sign Out' });
+		res.render('pages/index', { title: 'Home', username: req.session.username + '님', authLocation: 'signout', auth: '로그아웃' });
 	} else {
-		res.render('pages/index', { title: 'Home', username: '', authLocation: 'signin', auth: 'Sign In' });
+		res.render('pages/index', { title: 'Home', username: '', authLocation: 'signin', auth: '로그인' });
 	}
 });
 
