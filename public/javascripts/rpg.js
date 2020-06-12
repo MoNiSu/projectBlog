00f5402237f9100f5132c2eb8c8286e7c501805a
monisu_windows <xogns712015@gmail.com>
2020-06-12 21:55:40 +0900

    Add RPG javascript file

---
+++ public/javascripts/rpg.js
@@ -0,0 +1,19 @@
+class Character {
+	constructor (name, level) {
+		this.name = name;
+		this.level = level || 1;
+	}
+}
+
+class Hero extends Character {
+	constructor (props) {
+		super(props);
+		this.new = player;
+	}
+}
+
+class Monster extends Character {
+	constructor (props) {
+		super(props);
+	}
+}
